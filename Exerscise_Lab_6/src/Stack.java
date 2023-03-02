cba13df635a9b762dab1ff1c148e12461984ce88
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2023-03-02 15:41:22 +0200

    Stack

---
+++ Exerscise_Lab_6/src/Stack.java
@@ -0,0 +1,35 @@
+
+import java.util.Arrays;
+
+public class Stack {
+
+    int[] stack;
+    int currIdx;
+
+    public Stack(int size) {
+        this.stack = new int[size];
+        this.currIdx = 0;
+    }
+
+    public void push(int value) {
+        if (currIdx == stack.length) {
+            System.out.println("Stack FULL!");
+            return;
+        }
+        stack[currIdx] = value;
+        currIdx++;
+    }
+
+    public void pop() {
+        if (currIdx == 0) {
+            System.out.println("Stack empty -> can't pop!");
+            return;
+        }
+        stack[--currIdx] = 0;
+    }
+
+    public String toString() {
+        return Arrays.toString(stack).replaceAll(", ", "->");
+    }
+
+}
