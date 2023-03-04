ecab42f9b69c676a6037031af2d005110294a3e5
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2023-03-04 13:50:58 +0200

    code tester

--- DesignPatterns/src/App.java
+++ DesignPatterns/src/App.java
@@ -9,27 +9,47 @@ public class App {
 
     public static void main(String[] args) {
 
-        // List<Animal> zoo = new ArrayList<>();
-        // AnimalFactory factory = new AnimalFactory();
+        Logger console = Logger.getInstance();
 
-        // zoo.add(factory.getAnimal("Kuche"));
-        // zoo.add(factory.getAnimal("Kotka"));
-        // zoo.add(factory.getAnimal("Kuche"));
-        // zoo.add(factory.getAnimal("Kotka"));
+        // List<Animal> zoo = new ArrayList<>(45);
+        // //console.log(zoo.size());
+        // AnimalFactory factory = new AnimalFactory();
+        // Animal infAnimal;
+
+        // for(int i = 0; i < 10; i++){
+        // if (i%2 == 0 ){
+        // zoo.add(infAnimal = factory.getAnimal("Kuche"));
+        // } else {
+        // zoo.add(infAnimal = factory.getAnimal("KoTkA"));
+        // }
+        // console.infoLog(infAnimal.getClass() + " added to zoo list");
+        // }
+
+        // System.out.println(zoo);
 
         // for(Animal a : zoo){
         // System.out.println(a.name);
         // }
 
-        BikeBuilder builder = new BikeBuilder();
-        Bike myBike = builder.withBrand("TREK")
-                .withModel("PRO CALIBER")
-                .withFrameSize('M')
-                .withFrameMaterial("Carbon")
-                .withWheelSize(29)
-                .build();
+        // BikeBuilder builder = new BikeBuilder();
+        // Bike myBike = builder.withBrand("TREK")
+        // .withModel("PRO CALIBER")
+        // .withFrameSize('M')
+        // .withFrameMaterial("Carbon")
+        // .withWheelSize(29)
+        // .build();
 
-        System.out.println(myBike.toString());
+        // System.out.println(myBike.toString());
+
+        // System.err.println("eho");
+
+        console.emergencyLog("Emergency exit");
+        
+        Weekday day = Weekday.FRIDAY;
+        System.out.println(day.isWeekDay());
+        System.out.println(day.isHoliday());
+
+        
 
     }
 
