b5978b14820607bfa6e61755af7548430d80f596
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2023-03-20 12:48:36 +0200

    update

--- Files/src/App.java
+++ Files/src/App.java
@@ -1,22 +1,26 @@
 import java.io.File;
+import java.nio.file.Path;
+import java.util.Date;
 
 public class App {
     public static void main(String[] args) {
 
+
+        
         //File fileToSplit = new File("/Users/edko/Main/JavaCourse/Files/src/IMG_2171.jpeg");
 
-        File[] dir = {
-                new File("/Users/edko/Main/JavaCourse/Files/src/IMG_2171.jpeg_031"),
-                new File("/Users/edko/Main/JavaCourse/Files/src/IMG_2171.jpeg_033"),                
-                new File("/Users/edko/Main/JavaCourse/Files/src/IMG_2171.jpeg_033"),                
+        // File[] dir = {
+        //         new File("/Users/edko/Main/JavaCourse/Files/src/IMG_2171.jpeg_031"),
+        //         new File("/Users/edko/Main/JavaCourse/Files/src/IMG_2171.jpeg_033"),                
+        //         new File("/Users/edko/Main/JavaCourse/Files/src/IMG_2171.jpeg_033"),                
 
-        };
+        // };
 
-        try {
-            FileAppender.appendFiles(dir, "jpeg");
-        } catch (Exception e) {
-            System.out.println("IOException!");
-        }
+        // try {
+        //     FileAppender.appendFiles(dir, "jpeg");
+        // } catch (Exception e) {
+        //     System.out.println("IOException!");
+        // }
 
         // System.out.println(FilesTest.getFileSizeInBytes("/Users/edko/Main/JavaCourse/errors.txt"));
 
@@ -24,6 +28,21 @@ public class App {
         // System.out.println(FilesTest.getFileSizeInBytes("/Users/edko/Main/JavaCourse/errors.txt_2"));
         // System.out.println(FilesTest.getFileSizeInBytes("/Users/edko/Main/JavaCourse/errors.txt_3"));
         // System.out.println(FilesTest.getFileSizeInBytes("/Users/edko/Main/JavaCourse/errors.txt_4"));
+
+        //File test = new File("/Users/edko/Main/JavaCourse/Exerscise_Lab_2/src/BoxContainer.java");
+        // long lm = test.lastModified();
+        // Date date = new Date(lm);
+        //System.out.println(test.lastModified());
+        File test = new File("/Users/edko/Main/JavaCourse/Files/src/70014687318__EB2743CB-F743-43FB-BDF5-97C09C988396.jpeg");
+        
+
+        FileUtils.printFirstLine(test);
+        
+        // FileUtils.renameFile(test, "vrushtamegopak");
+        //System.out.println(FileUtils.deleteFile(test));
+
+        //System.out.println(test.getName());
+        //  System.out.println(FileUtils.getExtension(test));
 
     }
 }
